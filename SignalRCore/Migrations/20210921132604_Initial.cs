303a80a6fd8d6a0f2c9524e1cb715dc80ed80a2c
muratilgun <muratilgun34@gmail.com>
2021-09-21 16:54:31 +0300

    Added product views

---
+++ SignalRCore/Migrations/20210921132604_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SignalRCore.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProdId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProdName = table.Column<string>(nullable: true),
+                    Category = table.Column<string>(nullable: true),
+                    UnitPrice = table.Column<decimal>(nullable: false),
+                    StockQty = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProdId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
--- SignalRCore/wwwroot/js/site.js
+++ SignalRCore/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(() => {
+    LoadProdData();
+    function LoadProdData()
 
-// Write your JavaScript code.
+})
